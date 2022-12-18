d6e7e4fa9a4e91da89fafe7bbfe380cde3d17502
aazath <asaazath@gmail.com>
2022-12-18 11:41:12 +0530

    Second servlet with annotation is added

---
+++ FirstApp/src/main/java/com/ayana/controller/SecondServlet.java
@@ -0,0 +1,53 @@
+package com.ayana.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/second", loadOnStartup = 1)
+public class SecondServlet implements Servlet{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = res.getWriter();
+		out.println("<html><head></head>");
+		out.println("<body><h1>Welcome to Servlet with Annotation");
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+		
+	}
+
+}
